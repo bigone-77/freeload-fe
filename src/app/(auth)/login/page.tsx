1b138f469b5b5555008d391be4c6bcd5e6b02ce7
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2024-04-26 11:22:46 +0900

    🚧 Naver, kakao 로그인 모달 접근 및 성공시 url에 인가코드 전달

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,8 +1,30 @@
 'use client';
 
 import LoginButton from '@/components/LoginButton';
+import {
+  KAKAO_API_KEY,
+  KAKAO_REDIRECT_URI,
+  NAVER_CLIENT_ID,
+  NAVER_REDIRECT_URI,
+  STATE,
+} from '@/constants/SocialConst';
+import { useRouter } from 'next/navigation';
 
 export default function LoginPage() {
+  const router = useRouter();
+
+  const naverLoginHandler = () => {
+    window.location.href = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${STATE}&redirect_uri=${NAVER_REDIRECT_URI}`;
+  };
+
+  const kakaoLoginHandler = () => {
+    window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
+  };
+
+  const goHomeHandler = () => {
+    router.replace('/');
+  };
+
   return (
     <div
       className="absolute inset-0 bg-cover bg-center"
@@ -19,19 +41,19 @@ export default function LoginPage() {
         <div className="flex items-center justify-center flex-col gap-5 mt-20">
           <LoginButton
             label="네이버 로그인"
-            onClick={() => {}}
+            onClick={naverLoginHandler}
             theme="naver"
             social="naver"
           />
           <LoginButton
             label="카카오 로그인"
-            onClick={() => {}}
+            onClick={kakaoLoginHandler}
             theme="kakao"
             social="kakao"
           />
           <LoginButton
             label="로그인 없이 할래요"
-            onClick={() => {}}
+            onClick={goHomeHandler}
             theme="origin"
           />
         </div>
