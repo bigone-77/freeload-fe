cdda9b78211ab091565b9b6fc3a6eeee40291603
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2024-06-12 15:44:46 +0900

    🐛 Redirect config 삭제시 무한 리다이렉팅 버그

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,8 @@
 import { NextResponse } from 'next/server';
-import { auth } from './auth';
 
 export async function middleware() {
-  const session = await auth();
-  console.log(session);
-
-  if (!session?.user) {
-    const redirectUrl =
-      process.env.NODE_ENV === 'development'
-        ? `${process.env.NEXT_PUBLIC_DEV_URL}/login`
-        : `${process.env.NEXT_PUBLIC_PROD_URL}/login`;
-    return NextResponse.redirect(redirectUrl);
-  }
   return NextResponse.next();
 }
-// export const config = {
-//   matcher: ['/profile'],
-// };
+export const config = {
+  matcher: ['/profile'],
+};
