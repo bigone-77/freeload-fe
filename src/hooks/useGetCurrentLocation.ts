5a34904bf195e696fff1bb8dba6259d7ee0d28a5
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2024-06-14 11:20:22 +0900

    🐛 권한 재요청 버그 일차적으로 해결
    1.처음 요청 권한 보내기 2. 승인 시 persist에 lat, lng 등록되면서 홈으로 라우팅 3. 거부 시 위치 권한재요청(테스트할 부분임!)

--- src/hooks/useGetCurrentLocation.ts
+++ src/hooks/useGetCurrentLocation.ts
@@ -1,6 +1,7 @@
 /* eslint-disable consistent-return */
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+
 import { setCurrentUserLocation } from '@/shared/store/slices/getCurrentLocationSlice';
 import { setMapCenterLocation } from '@/shared/store/slices/getMapCenterSlice';
 
@@ -8,53 +9,76 @@ export const useGetCurrentLocation = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    const handlePermissionDenied = () => {
+      // TODO: 권한 거부시 마운트될 때 다시 한 번 더 물어보기
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          const currentPos = {
+            latitude: pos.coords.latitude,
+            longitude: pos.coords.longitude,
+          };
+
+          dispatch(
+            setCurrentUserLocation({
+              latitude: currentPos.latitude,
+              longitude: currentPos.longitude,
+            }),
+          );
+          dispatch(
+            setMapCenterLocation({
+              latitude: currentPos.latitude,
+              longitude: currentPos.longitude,
+            }),
+          );
+        },
+        (err) => {
+          console.log(err);
+        },
+      );
+    };
+
     const checkPermissionAndWatchPosition = async () => {
-      const storedPermission = localStorage.getItem('geoPermission');
-
-      if (storedPermission === 'denied') {
-        alert(
-          '위치 권한이 거부되었습니다. 설정에서 위치 권한을 허용해 주세요.',
-        );
-        return;
-      }
-
-      const watchPosition = async () => {
+      try {
         const permissionStatus = await navigator.permissions.query({
           name: 'geolocation',
         });
+        console.log(permissionStatus.state);
 
-        if (
-          permissionStatus.state === 'granted' ||
-          permissionStatus.state === 'prompt'
-        ) {
-          const watchId = navigator.geolocation.watchPosition(
-            (pos) => {
-              const currentPos = {
-                latitude: pos.coords.latitude,
-                longitude: pos.coords.longitude,
-              };
-
-              dispatch(setCurrentUserLocation(currentPos));
-              dispatch(setMapCenterLocation(currentPos));
-            },
-            (err) => {
-              console.log(err);
-            },
-          );
-
-          return () => {
-            navigator.geolocation.clearWatch(watchId);
-          };
-        }
         if (permissionStatus.state === 'denied') {
-          alert(
-            '위치 권한이 거부되었습니다. 설정에서 위치 권한을 허용해 주세요.',
-          );
-          localStorage.setItem('geoPermission', 'denied');
+          handlePermissionDenied();
         }
-      };
 
-      watchPosition();
+        const watchId = navigator.geolocation.watchPosition(
+          (pos) => {
+            const currentPos = {
+              latitude: pos.coords.latitude,
+              longitude: pos.coords.longitude,
+            };
+
+            dispatch(
+              setCurrentUserLocation({
+                latitude: currentPos.latitude,
+                longitude: currentPos.longitude,
+              }),
+            );
+            dispatch(
+              setMapCenterLocation({
+                latitude: currentPos.latitude,
+                longitude: currentPos.longitude,
+              }),
+            );
+          },
+          (err) => {
+            console.log(err);
+          },
+        );
+
+        return () => {
+          navigator.geolocation.clearWatch(watchId);
+        };
+      } catch (err) {
+        console.error('위치 권한 조회 실패:', err);
+      }
     };
 
     checkPermissionAndWatchPosition();
