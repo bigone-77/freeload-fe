b10a3ee09ef8b5ee3945a815ac37e7be1f45a4f2
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2024-06-20 23:29:00 +0900

    🐛 Kakao map 안불러와지는 버그 src 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,7 +49,7 @@ export default async function RootLayout({
           </RQProvider>
         </AuthSession>
         <Script
-          src={`dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_KEY}&libraries=services,clusterer&autoload=false`}
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_KEY}&libraries=services,clusterer&autoload=false`}
           strategy="beforeInteractive"
         />
         <Script
