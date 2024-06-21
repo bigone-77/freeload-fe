bca05ee6cb93e24736b4a49a2f796630b3412a54
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2024-06-21 15:30:56 +0900

    🧪 기본으로 열리는 page.tsx가 csr일때 에러가 나오는건지 확인

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import Onboarding from './_components/onboarding';
+// import Onboarding from './_components/onboarding';
 
 export default function Page() {
-  return <Onboarding />;
+  // return <Onboarding />;
+  return <p>이러면 오류 안나지롱</p>;
 }
