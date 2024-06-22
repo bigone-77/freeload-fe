1281b08a7b7acc72006f337748f6110c67fab648
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2024-06-22 12:20:52 +0900

    🐛 Api/rest/all route.ts query string 제거

--- src/app/api/rest/all/route.ts
+++ src/app/api/rest/all/route.ts
@@ -1,14 +1,9 @@
 import axios from 'axios';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
-
-  const timeStamp = searchParams.get('timestamp');
+export async function GET() {
   try {
-    const response = await axios(
-      `${process.env.NEXT_PUBLIC_BE_URL}/rest/all?timestamp=${timeStamp}`,
-    );
+    const response = await axios(`${process.env.NEXT_PUBLIC_BE_URL}/rest/all`);
 
     if (response.status === 200) {
       return NextResponse.json(response.data, { status: 200 });
